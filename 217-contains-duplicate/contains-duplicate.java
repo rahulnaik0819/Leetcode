cb906c3334df21a94a944f3fc7777f30d6059fd1
Rahulnaik08 <rahulnaikd0819@gmail.com>
2026-09-16 01:24:28 +0530

    Time: 18 ms (76.87%) | Memory: 108.5 MB (23.15%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,19 +1,14 @@
 import java.util.HashSet;
-
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-
         HashSet<Integer> set = new HashSet<>();
 
-        for(int i = 0; i < nums.length; i++) {
-
-            if(set.contains(nums[i])) {
+        for(int num : nums){
+            if(set.contains(num)){
                 return true;
             }
-
-            set.add(nums[i]);
+            set.add(num);
         }
-
         return false;
     }
 }
